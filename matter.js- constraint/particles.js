cec9693df0810250edbf6a0394db74959cf238ad
Sam <63341022+whereissam@users.noreply.github.com>
2021-01-21 21:14:13 +0800

    Matter.js constraint

---
+++ matter.js- constraint/particles.js
@@ -0,0 +1,40 @@
+function Particle(x,y,r,fixed){
+    var options = {
+        friction: 0,
+        restitution: 0.95,
+        isStatic: fixed
+    }
+    this.body = Bodies.circle(x,y,r, options)
+    this.body.friction = 0
+    this.r = r
+    World.add(world, this.body)
+
+    
+    this.isOffScreen = function(){
+        var pos = this.body.position;
+        if(pos.y > height + 100){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    this.removeFromWorld = function(){
+        World.remove(world, this.body)
+    }
+
+    this.show = function(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+            translate(pos.x, pos.y)
+            rotate(angle);
+            rectMode(CENTER);
+            strokeWeight(1);
+            stroke(255);
+            fill(random(255),random(255),random(255));
+            ellipse(0,0,this.r*1.9)
+        pop();
+    }
+}
